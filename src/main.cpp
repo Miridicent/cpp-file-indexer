a3b2dd9a883f9a0ef93be499710c8408dd933c5c
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2026-08-27 16:18:09 +0100

    Error checking
    The indexer recognises when a directory is not given, if a file isn't a directory and if the directory does not exist.

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,42 @@ int main(int argc, char* argv[])
         std::cout << "Argument " << i << ": " << argv[i] << std::endl;
     }
 
+    if (argc < 2)
+    {
+        std::cout << "Error: Please provide a directory path." << std::endl;
+        return 1;
+    }
+
     std::filesystem::path directoryPath = argv[1];
     
-
-    if (std::filesystem::exists(directoryPath))
+    if (!std::filesystem::exists(directoryPath))
     {
-        std::cout << "Directory exists." << std::endl;
+        std::cout << "Error! Directory not found." << std::endl;
+    }
+
+    else if (!std::filesystem::is_directory(directoryPath))
+    {
+        std::cout << "Error! This is not a directory." << std::endl;
+    }
+
+    else
+    {
+        std::cout << "Directory found." << std::endl;
 
         std::filesystem::directory_iterator iterator(directoryPath);
 
         for (const auto& entry : iterator)
         {
-            std::cout << entry.path() << std::endl;
+            if (entry.is_directory())
+            {
+                std::cout << "Directory: " << entry.path() << std::endl;
+                
+            }
+            else 
+            {
+                std::cout << "File: " << entry.path() << std::endl;
+            }
         }
-    }
-
-    else 
-    {
-        std::cout << "Error! Directory not found." << std::endl;
     }
 
     return 0;
